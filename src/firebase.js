4add9fea07b7ab54b738a1c9a827782aaaa0d903
AniketR1209 <102343656+AniketR1209@users.noreply.github.com>
2023-01-06 16:44:15 +0530

    first commit

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCyohgPqaX4D-a4Eka7ZwJoehZPKLBSliE",
-  authDomain: "phone-auth-ec6a3.firebaseapp.com",
-  projectId: "phone-auth-ec6a3",
-  storageBucket: "phone-auth-ec6a3.appspot.com",
-  messagingSenderId: "202357694589",
-  appId: "1:202357694589:web:36ff216ea688064fe21114"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
